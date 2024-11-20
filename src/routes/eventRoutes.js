8172cd6e00597adde2f2d4120e1e6cefed1b83ff
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2024-11-21 01:40:37 +0200

    add event routes

---
+++ src/routes/eventRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import * as EventController from '../controllers/eventController.js';
+
+const router = express.Router();
+
+// Routes
+router.post('/', EventController.createEvent);
+router.get('/:userId', EventController.getEventsByUserId);
+router.get('/:userId/state', EventController.getUserConsentState);
+
+export default router;
