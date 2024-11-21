925f8414668761487e359f516bbb25c3dda5f513
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2024-11-21 14:24:43 +0200

    apply the input validation middleware in userRoutes.js

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import * as EventController from "../controllers/eventController.js";
-import { validateUserInput } from "../middleware/validateUserInput.js";
 
 const router = express.Router();
 
 // Routes
-router.post("/", validateUserInput, EventController.createEvent);
+router.post("/", EventController.createEvent);
 router.get("/:userId", EventController.getEventsByUserId);
 router.get("/:userId/state", EventController.getUserConsentState);
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import * as UserController from "../controllers/userController.js";
+import { validateUserInput } from "../middleware/validateUserInput.js";
 
 const router = express.Router();
 
 // routes
-router.post("/", UserController.createUser);
+router.post("/", validateUserInput, UserController.createUser);
 router.get("/", UserController.getAllUsers);
 router.get("/:id", UserController.getUserById);
 router.put("/:id", UserController.updateUserEmail);
 router.delete("/:id", UserController.deleteUser);
-
 
 export default router;
